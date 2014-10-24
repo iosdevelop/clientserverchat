aeddd86d0dd26672d020efc616c2571747f8f413
PSanf2 <patricksanford@live.com>
2014-10-24 16:46:55 -0400

    Corrected a setup error
    It helps to setup a SWT project if we want to make a GUI.

---
+++ eclipse workspace/CSC_440_Group_Project/src/client/Client.java
@@ -0,0 +1,26 @@
+package client;
+
+import shared.Message;
+
+public class Client extends Thread {
+	
+	public Client() {
+		
+	}
+	
+	public synchronized void connect() {
+		
+	}
+	
+	public synchronized void disconnect() {
+		
+	}
+	
+	public void run() {
+		
+	}
+	
+	public synchronized void send_message(Message message) {
+		
+	}
+}
